3cc7c7d141450d1cb055d57fd14e97dfe3582ac1
Lmickk2 <lukemickan21@icloud.com>
2022-11-10 06:41:32 -0500

    restructured UI, added map API, changed container content.

--- scripts.js
+++ scripts.js
@@ -1,18 +1,102 @@
-var displayName = document.querySelector('.main-name');
-var dateResult = document.querySelector('.date-list');
-var eventResult = document.querySelector('.event');
-var search = document.querySelector('.searchBtn');
-var dateSelect = document.querySelector('.date');
-var destination = document.querySelector('.destination');
+function initMap() {
+    var options = {
+        center: {lat: 25.7602 , lng: -80.1959 },
+        zoom: 14,
+        styles: [
+            { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
+            { elementType: "labels.text.stroke", stylers: [{ color: "#242f3e" }] },
+            { elementType: "labels.text.fill", stylers: [{ color: "#746855" }] },
+            {
+              featureType: "administrative.locality",
+              elementType: "labels.text.fill",
+              stylers: [{ color: "#d59563" }],
+            },
+            {
+              featureType: "poi",
+              elementType: "labels.text.fill",
+              stylers: [{ color: "#d59563" }],
+            },
+            {
+              featureType: "poi.park",
+              elementType: "geometry",
+              stylers: [{ color: "#263c3f" }],
+            },
+            {
+              featureType: "poi.park",
+              elementType: "labels.text.fill",
+              stylers: [{ color: "#6b9a76" }],
+            },
+            {
+              featureType: "road",
+              elementType: "geometry",
+              stylers: [{ color: "#38414e" }],
+            },
+            {
+              featureType: "road",
+              elementType: "geometry.stroke",
+              stylers: [{ color: "#212a37" }],
+            },
+            {
+              featureType: "road",
+              elementType: "labels.text.fill",
+              stylers: [{ color: "#9ca5b3" }],
+            },
+            {
+              featureType: "road.highway",
+              elementType: "geometry",
+              stylers: [{ color: "#746855" }],
+            },
+            {
+              featureType: "road.highway",
+              elementType: "geometry.stroke",
+              stylers: [{ color: "#1f2835" }],
+            },
+            {
+              featureType: "road.highway",
+              elementType: "labels.text.fill",
+              stylers: [{ color: "#f3d19c" }],
+            },
+            {
+              featureType: "transit",
+              elementType: "geometry",
+              stylers: [{ color: "#2f3948" }],
+            },
+            {
+              featureType: "transit.station",
+              elementType: "labels.text.fill",
+              stylers: [{ color: "#d59563" }],
+            },
+            {
+              featureType: "water",
+              elementType: "geometry",
+              stylers: [{ color: "#17263c" }],
+            },
+            {
+              featureType: "water",
+              elementType: "labels.text.fill",
+              stylers: [{ color: "#515c6d" }],
+            },
+            {
+              featureType: "water",
+              elementType: "labels.text.stroke",
+              stylers: [{ color: "#17263c" }],
+            },
+          ],
+        }
 
 
-search.addEventListener('click', function(citySearch){
-    fetch('https://opentable.herokuapp.com/api/restaurants?=cities?=miami')
-    .then(response => response.json())
-    .then(data => {
-      console.log(data)
+    
 
-        
-    })
-    })
+map = new google.maps.Map(document.getElementById("map"),options)
+
+}
+// var displayName = document.querySelector('.main-name');
+// var dateResult = document.querySelector('.date-list');
+// var eventResult = document.querySelector('.event');
+// var search = document.querySelector('.searchBtn');
+// var dateSelect = document.querySelector('.date');
+// var destination = document.querySelector('.destination');
+
+
+// search.addEventListener('click', function(citySearch){
 
